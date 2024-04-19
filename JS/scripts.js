bef86ea9ef8ebd84aa547569020371a761efd605
Skelli75 <alfred.valentin.krantz@gmail.com>
2024-04-19 14:40:05 +0200

    Continued work on loading in from json

--- JS/scripts.js
+++ JS/scripts.js
@@ -15,21 +15,40 @@ searchButton.addEventListener('click', async function()
     /*const response = await fetch(url, options);
     const result = await response.json();*/
 
-    const result = require('../film.json');
+    const response = await fetch('../JSON/film.json');
+    const result = await response.json();
+
+    const gridArea = ["a","b","c"];
+
+    let childAmount = 0;
+    let parentAmount = 0;
 
     for (let i = 0; i < result.data.length; i++)
     {
-        const movieDiv = 
-        "<div class='movie'>" +
-            "<img class='movieImgContainer' src=" + result.data[i].image + " alt=''>" +
-            "<div>" + 
-                "<h2 class='movieTitle'>" + result.data[i].title + "</h2>" +
-                "<p>Year: " + result.data[i].year + "</p>" +
-                "<p>Stars: " + result.data[i].stars + "</p>" +                        
-            "</div>" +
-        "</div>";
+        
 
-        movieSearchResultDiv.insertAdjacentHTML("beforeend", movieDiv);
+        if (childAmount < 3)
+        {
+            const movieDiv = 
+            "<div class='movie' style='grid-area:" + gridArea[childAmount] + ";' >" +
+                "<img class='movieImgContainer' src=" + result.data[i].image + " alt=''>" +
+                "<div>" + 
+                    "<h2 class='movieTitle'>" + result.data[i].title + "</h2>" +
+                    "<p>Year: " + result.data[i].year + "</p>" +
+                    "<p>Stars: " + result.data[i].stars + "</p>" +                        
+                "</div>" +
+            "</div>";
 
+            movieSearchResultDiv.children[parentAmount].insertAdjacentHTML("beforeend", movieDiv);
+            childAmount = childAmount + 1;
+        }
+        else 
+        {
+            const movieParentDiv =
+            "<div class='movieParent'></div>"
+            movieSearchResultDiv.insertAdjacentHTML("beforeend", movieParentDiv);
+            childAmount = 0;
+            parentAmount = parentAmount + 1;
+        }
     }
 });
