e82c21b415dbef5036e2fae1e02269a996ab8e70
Skelli75 <alfred.valentin.krantz@gmail.com>
2024-05-19 02:53:30 +0200

    Small fixes, need to fix arrows not changing size in chromium based browsers

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,12 +1,12 @@
 const searchButton = document.getElementById('SearchButton');
 const searchResultDiv = document.getElementById('SearchResultDiv');
 
-searchButton.addEventListener('click', async function()
+searchButton.addEventListener('click', async function() //fyller sökresultaten med filmer
 {
-    EmptyResults();
+    EmptyResults(); 
 
 
-    const searchquery = document.getElementById('SearchInput').value.replace(/\s/g, "");
+    const searchquery = document.getElementById('SearchInput').value.replace(/\s/g, ""); //tar bort mellanrum i sökorden
 
 
     const url = 'https://imdb188.p.rapidapi.com/api/v1/searchIMDB?query=' + searchquery;
@@ -24,25 +24,22 @@ searchButton.addEventListener('click', async function()
     /*const response = await fetch('JSON/horror.json');*/ /*test för att inte använda upp alla api anrop*/ 
     /*const result = await response.json();*/
 
-    let parentAmount = 1;
-
     for (let i = 0; i < result.data.length; i++)
     {
-        let parentDiv = searchResultDiv.children[parentAmount];
+        let parentDiv = searchResultDiv.children[searchResultDiv.children.length - 1];
 
-        if (parentDiv.children.length == 3) 
+        if (parentDiv.children.length == 3) //Lägger till en ny förälder om den aktuella föräldern innehåller 3 barn
         {
             const resultParentDiv =
             "<div class='resultParent'></div>"
             searchResultDiv.insertAdjacentHTML("beforeend", resultParentDiv);
-            parentAmount++;
 
-            parentDiv = searchResultDiv.children[parentAmount];
+            parentDiv = searchResultDiv.children[searchResultDiv.children.length - 1];
         }
-        if (parentDiv.children.length < 3)
+        if (parentDiv.children.length < 3) //om föräldern inte är full
         {
 
-            if (result.data[i].id[0] == "t" || result.data[i].id[0] == "/")
+            if (result.data[i].id[0] == "t" || result.data[i].id[0] == "/") //Kollar om resultatet är en film 
             {
                 const movieDiv = 
                 "<div class='result'>" +
@@ -55,7 +52,7 @@ searchButton.addEventListener('click', async function()
 
                 parentDiv.insertAdjacentHTML("beforeend", movieDiv);
             }
-            else if (result.data[i].id[0] == "n")
+            else if (result.data[i].id[0] == "n") //Kollar om resultatet är en skådespelare
             {
                 const actorDiv = 
                 "<div class='result'>" +
@@ -75,7 +72,7 @@ searchButton.addEventListener('click', async function()
 });
 
 
-async function Top10()
+async function Top10() //fyller top10 med resultat
 {
     const top10Div = document.getElementById("top10Div");
 
@@ -88,28 +85,26 @@ async function Top10()
         }
     };
 
-    const response = await fetch(url, options); 
+    /*const response = await fetch(url, options); 
+    const result = await response.json();*/
+
+    /*test för att inte använda upp alla api anrop*/
+    const response = await fetch('JSON/weektop10.json'); 
     const result = await response.json();
-
-    /*const response = await fetch('JSON/weektop10.json');*/ /*test för att inte använda upp alla api anrop*/
-    /*const result = await response.json();*/
-
-    let parentAmount = 1;
 
     for (let i = 0; i < result.data.length; i++)
     {
-        let parentDiv = top10Div.children[parentAmount];
+        let parentDiv = top10Div.children[top10Div.children.length - 1];
 
-        if (parentDiv.children.length == 2) 
+        if (parentDiv.children.length == 2) //skapar en ny förälder om den senaste föräldern är full
         {
             const resultParentDiv =
             "<div class='resultParent'></div>"
             top10Div.insertAdjacentHTML("beforeend", resultParentDiv);
-            parentAmount++;
 
-            parentDiv = top10Div.children[parentAmount];
+            parentDiv = top10Div.children[top10Div.children.length - 1];
         }
-        if (parentDiv.children.length < 2)
+        if (parentDiv.children.length < 2) //om den aktulle föräldern inte är full
         {
             const movieDiv = 
             "<div class='result'>" +
@@ -124,30 +119,19 @@ async function Top10()
             AddImg(parentDiv.children[parentDiv.children.length - 1], result.data[i].primaryImage.imageUrl);
         }
     }
-
-    while (true)
-    {
-        if  (top10Div.children[parentAmount].children.length < 2)
-        {
-            const movieDiv = "<div class='result'> </div>"
-            top10Div.children[parentAmount].insertAdjacentHTML("beforeend", movieDiv);
-        }
-        else
-            break;
-    }
 };
 
 
 
-function AddImg(div, img)
+function AddImg(div, img) 
 {
     let imgDiv;
 
-    if (img === undefined)
+    if (img === undefined) //om bilden är undefined ersätts den med en standard bild
     {
         imgDiv = "<img class='resultImgContainer' src='../Resources/No-Image-Placeholder.svg' alt=''>";
     }
-    else 
+    else //Om bilden inte är undefined läggs den till i diven
     {
         imgDiv = "<img class='resultImgContainer' src=" + img + " alt=''>";
     }
@@ -155,7 +139,7 @@ function AddImg(div, img)
     div.insertAdjacentHTML("afterbegin", imgDiv)
 };
 
-function EmptyResults()
+function EmptyResults() //Tömmer searchresultdiven och fyller den med standard värdena.
 {
     searchResultDiv.innerHTML = "";
     searchResultDiv.insertAdjacentHTML("beforeend", "<Button class='collapsible' style='color: white;''><img src='Resources/right-arrow-small.png' alt='right-arrow-small'><h1>Search results:</h1></Button>")
@@ -163,14 +147,14 @@ function EmptyResults()
 };
 
 
-document.addEventListener('DOMContentLoaded', function() 
+document.addEventListener('DOMContentLoaded', function() //laddar in top10 när sidan laddats in
 {
     Top10();
     SetUpCollapsible();
 });
 
 
-function SetUpCollapsible()
+function SetUpCollapsible() //gör så att resultaten går att gömma med en knapp
 {
     var coll = document.getElementsByClassName("collapsible");
     var i;
@@ -202,12 +186,12 @@ function SetUpCollapsible()
     }
 };
 
-function PointCollapsibleImgUp(img)
+function PointCollapsibleImgUp(img) //vänder pilen som representerar collapsible knappen uppåt
 {
     img.style.transform = 'rotate(180deg)';
 };
 
-function PointCollapsibleImgDown(img)
+function PointCollapsibleImgDown(img) //vänder den neråt
 {
     img.style.transform = 'rotate(0deg)';
 };
